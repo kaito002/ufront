db4d9207aafa7cb48cf12ca2c924c9c508cf6e88
Kaito <carlosmontes002@gmail.com>
2020-07-12 03:39:28 -0300

    Rebuild Library

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -3,7 +3,14 @@ import React from 'react'
 import { UFront } from 'ufront'
 
 const App = () => {
-  return <UFront host="http://localhost" name="MicroSection" renderMethodName="renderMicroSection" />
+  return (
+    <div>
+      <h2 style={{textAlign: "center"}}>Welcome to Micro Frontend World, here a independent section!!</h2>
+      <div style={{display: "flex", justifyContent:"center"}}>
+        <UFront host="http://localhost:8080" name="MicroSection" />
+      </div>
+    </div>
+  )
 }
 
-export default App
+export default App;
