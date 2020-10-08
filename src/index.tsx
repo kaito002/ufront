aab3dc4f9530007ee8f144390e73856d2590bc20
Kaito <carlosmontes002@gmail.com>
2020-10-07 22:05:10 -0300

    remove public in npm publish

--- src/index.tsx
+++ src/index.tsx
@@ -1,103 +1,107 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react'
+
 interface UFrontProps {
-  name: string,
-  host: string,
-  renderMethodName?: string
+  name: string
+  host: string
 }
 
-const removeContainerHost = (url: string) => url.replace(window.location.origin, "");
+const removeContainerHost = (url: string) =>
+  url.replace(window.location.origin, '')
 
-const nodesToArray = (nodes?: HTMLCollection) => Array.prototype.slice.call(nodes || new HTMLCollection());
+const nodesToArray = (nodes?: HTMLCollection) =>
+  Array.prototype.slice.call(nodes || new HTMLCollection())
 
-export const UFront = ({ name, host }: UFrontProps) => {
-  const containerId = `${name}-container`;  
+const UFront = ({ name, host }: UFrontProps) => {
+  const containerId = `${name}-container`
 
-  const [loadingError, setLoadingError] = useState(false);
+  const [loadingError, setLoadingError] = useState(false)
 
   const loadComponent = () => {
     renderMicroSection()
-    
-    const container = document.getElementById(containerId);
+
+    const container = document.getElementById(containerId)
 
     const mediaElements = [
-      ...nodesToArray(container?.getElementsByTagName("img")),
-      ...nodesToArray(container?.getElementsByTagName("video")),
-      ...nodesToArray(container?.getElementsByTagName("audio"))
+      ...nodesToArray(container?.getElementsByTagName('img')),
+      ...nodesToArray(container?.getElementsByTagName('video')),
+      ...nodesToArray(container?.getElementsByTagName('audio'))
     ]
 
-    mediaElements.forEach(element => {
+    mediaElements.forEach((element) => {
       if (!element.src.startsWith(host)) {
         element.src = `${host}${removeContainerHost(element.src)}`
       }
-    });
+    })
   }
 
   const loadBundle = () => {
-    const script = document.createElement("script");
+    const script = document.createElement('script')
 
-    script.id = `${name}-bundle`;
-    script.src = `${host}/micro-section`
-    script.crossOrigin = "";
-    script.async = true;
-    script.onerror = onError;
-    script.onload = loadComponent;
-    document.getElementById(containerId)?.appendChild(script);
+    script.id = `${name}-bundle`
+    script.src = `${host}`
+    script.crossOrigin = ''
+    script.async = true
+    script.onerror = onError
+    script.onload = loadComponent
+    document.getElementById(containerId)?.appendChild(script)
   }
 
   const onError = () => {
-    setLoadingError(true);
-   console.error(`Error Getting ${name} frontend: `);
+    setLoadingError(true)
+    console.error(`Error Getting ${name} frontend: `)
   }
 
-  const renderMicroSection = () =>{
-    window[`render${name}`](containerId);
+  const renderMicroSection = () => {
+    window[`render${name}`](containerId)
   }
 
   const unmountMicroSection = () => {
     if (window.hasOwnProperty(`unmount${name}`)) {
-      window[`unmount${name}`](containerId) 
+      window[`unmount${name}`](containerId)
     }
   }
 
-  useEffect(() => { 
+  useEffect(() => {
     if (window.hasOwnProperty(`render${name}`)) {
       loadComponent()
     } else {
-      loadBundle() 
+      loadBundle()
     }
-  }, []);
+  }, [])
 
   useEffect(() => {
     return () => {
-     unmountMicroSection();
+      unmountMicroSection()
     }
   })
 
   if (loadingError) {
     return (
-      <div style={ufrontStyles.container}>
-        <span style={ufrontStyles.text}>An Error produced loading &lt;&lt;{name}&gt;&gt; micro-frontend</span>
+      <div style={uFrontStyles.container}>
+        <span style={uFrontStyles.text}>
+          An Error produced loading &lt;&lt;{name}&gt;&gt; micro-frontend
+        </span>
       </div>
     )
   }
 
-  return (
-    <section id={containerId}></section>
-  )
+  return <section id={containerId} />
 }
 
-//Styles
-const ufrontStyles = {
+export default { UFront }
+
+// Styles
+const uFrontStyles = {
   container: {
-    backgroundColor: "#ff5722",
-    border: "solid",
+    backgroundColor: '#ff5722',
+    border: 'solid',
     borderWidth: 2,
-    borderColor: "#dd2c00",
+    borderColor: '#dd2c00',
     padding: 10
   },
 
   text: {
     fontSize: 25,
-    color: "#FFF"
+    color: '#FFF'
   }
 }
